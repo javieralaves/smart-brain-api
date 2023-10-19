251c863ee5c4326ca88decd249b24821b5412aa7
Javier Alaves <jayalaves@gmail.com>
2023-10-19 12:35:20 +0200

    getting profiles by id endpoint

--- server.js
+++ server.js
@@ -81,16 +81,18 @@ app.post('/register', (req, res) => {
 // get user for profile
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params;
-	let found = false;
-	database.users.forEach((user) => {
-		if (user.id === id) {
-			found = true;
-			return res.json(user);
-		}
-	});
-	if (!found) {
-		res.status(404).json("couldn't find the user");
-	}
+	db.select('*')
+		.from('users')
+		.where({ id })
+		.then((user) => {
+			// check if array isn't empty, which would mean there's no user for a given id
+			if (user.length) {
+				res.json(user[0]);
+			} else {
+				res.status(400).json('Not found');
+			}
+		})
+		.catch((err) => res.status(400).json('Error getting user'));
 });
 
 // listen to server on port 3000
